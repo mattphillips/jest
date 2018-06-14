d4db18c4fd9d9e37228e053d1c1096860f9558f3
Matt Phillips <matt@mattphillips.io>
2018-06-14 09:12:34 +0100

    WIP: repalce util format with $ notation

--- packages/jest-each/src/bind.js
+++ packages/jest-each/src/bind.js
@@ -7,21 +7,14 @@
  * @flow
  */
 
-import util from 'util';
 import chalk from 'chalk';
 import pretty from 'pretty-format';
 import {getPath} from 'jest-util';
 
 type Table = Array<Array<any>>;
-type PrettyArgs = {
-  args: Array<mixed>,
-  title: string,
-};
 
 const EXPECTED_COLOR = chalk.green;
 const RECEIVED_COLOR = chalk.red;
-const SUPPORTED_PLACEHOLDERS = /%[sdifjoOp%]/g;
-const PRETTY_PLACEHOLDER = '%p';
 
 export default (cb: Function) => (...args: any) =>
   function eachBind(title: string, test: Function): void {
@@ -30,7 +23,7 @@ export default (cb: Function) => (...args: any) =>
         ? args[0]
         : args[0].map(entry => [entry]);
       return table.forEach(row =>
-        cb(arrayFormat(title, ...row), applyRestParams(row, test)),
+        cb(interpolate(title, row), applyRestParams(row, test)),
       );
     }
 
@@ -69,43 +62,6 @@ export default (cb: Function) => (...args: any) =>
       cb(interpolate(title, row), applyObjectParams(row, test)),
     );
   };
-
-const getPrettyIndexes = placeholders =>
-  placeholders.reduce(
-    (indexes, placeholder, index) =>
-      placeholder === PRETTY_PLACEHOLDER ? indexes.concat(index) : indexes,
-    [],
-  );
-
-const arrayFormat = (title, ...args) => {
-  const placeholders = title.match(SUPPORTED_PLACEHOLDERS) || [];
-  const prettyIndexes = getPrettyIndexes(placeholders);
-
-  const {title: prettyTitle, args: remainingArgs} = args.reduce(
-    (acc: PrettyArgs, arg, index) => {
-      if (prettyIndexes.indexOf(index) !== -1) {
-        return {
-          args: acc.args,
-          title: acc.title.replace(
-            PRETTY_PLACEHOLDER,
-            pretty(arg, {maxDepth: 1, min: true}),
-          ),
-        };
-      }
-
-      return {
-        args: acc.args.concat([arg]),
-        title: acc.title,
-      };
-    },
-    {args: [], title},
-  );
-
-  return util.format(
-    prettyTitle,
-    ...remainingArgs.slice(0, placeholders.length - prettyIndexes.length),
-  );
-};
 
 const applyRestParams = (params: Array<any>, test: Function) => {
   if (params.length < test.length) return done => test(...params, done);
